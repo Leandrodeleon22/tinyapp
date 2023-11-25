5a50966a3d6347b69a903eb2d477f8e0d668bdcc
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-25 11:04:13 -0500

    feat: implement post request, redirect, and redirect through route /u/:id

--- express_server.js
+++ express_server.js
@@ -11,6 +11,19 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = (length) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,13 +39,25 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
-  const templateVars = { id, longURL: "http://www.lighthouselabs.ca" };
+  const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("ok!");
+app.get("/u/:id", (req, res) => {
+  const { id } = req.params;
+  const longURL = urlDatabase[id];
+
+  // console.log(longURL);
+  res.redirect(longURL);
+});
+
+app.post("/urls/", (req, res) => {
+  const id = generateRandomString(6);
+  const { longURL } = req.body;
+  Object.assign(urlDatabase, { [id]: longURL });
+
+  // res.redirect(`urls/${id}`);
+  res.redirect(`/urls/${id}`);
 });
 
 // app.get("/urls.json", (req, res) => {
