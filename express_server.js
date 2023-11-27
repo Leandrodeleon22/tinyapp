2f37ff8b1a07b3dc788689fd2c16b06c5e0f7619
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-27 06:25:26 -0500

    feat: Add login route, add login component in the header and set cookie

--- express_server.js
+++ express_server.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//MIDLEWARE
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.use(express.urlencoded({ extended: true }));
-
+//GET RANDOM ID
 const generateRandomString = (length) => {
   let result = "";
   const characters =
@@ -24,39 +25,43 @@ const generateRandomString = (length) => {
   return result;
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+//HOMEPAGE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//CREATE NEW URL PAGE
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+  // res.redirect("/urls");
 });
 
-app.post("/urls/:id/delete", (req, res) => {
-  const { id: idToDelete } = req.params;
-  delete urlDatabase[idToDelete];
-  res.redirect("/urls");
-});
-
+//EDIT PAGE
 app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
   const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
 
+//CREATE PAGE
 app.get("/u/:id", (req, res) => {
   const { id } = req.params;
   const longURL = urlDatabase[id];
 
-  // console.log(longURL);
   res.redirect(longURL);
 });
 
+//EDIT
+app.post("/urls/", (req, res) => {
+  const id = generateRandomString(6);
+  const { longURL } = req.body;
+  Object.assign(urlDatabase, { [id]: longURL });
+
+  res.redirect(`/urls/${id}`);
+});
+
+//CREATE
 app.post("/urls/", (req, res) => {
   const id = generateRandomString(6);
   const { longURL } = req.body;
@@ -64,6 +69,34 @@ app.post("/urls/", (req, res) => {
 
   // res.redirect(`urls/${id}`);
   res.redirect(`/urls/${id}`);
+});
+
+//DELETE
+app.post("/urls/:id/delete", (req, res) => {
+  const { id: idToDelete } = req.params;
+  delete urlDatabase[idToDelete];
+  res.redirect("/urls");
+});
+
+//UPDATE
+app.post("/urls/:id", (req, res) => {
+  const { id } = req.params;
+  const { editURL } = req.body;
+  urlDatabase[id] = editURL;
+
+  res.redirect("/urls");
+  // urlDatabase[id] =
+});
+
+//LOGIN
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const { username: usernameValue } = req.body;
+
+  res.cookie("username", usernameValue);
+  res.redirect("/urls");
 });
 
 // app.get("/urls.json", (req, res) => {
