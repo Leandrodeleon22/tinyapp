e9e3bacd9c5e33e0ace21d7570727a290651b241
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-29 08:24:37 -0500

    feat: add register form, register route , add users, add new user, and display user email in header

--- express_server.js
+++ express_server.js
@@ -13,26 +13,48 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 //GET RANDOM ID
+// const generateRandomString = (length) => {
+//   let result = "";
+//   const characters =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   const charactersLength = characters.length;
+//   let counter = 0;
+//   while (counter < length) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     counter += 1;
+//   }
+//   return result;
+// };
+
 const generateRandomString = (length) => {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
+  let result = Math.random().toString(36).substr(2, length);
   return result;
 };
 
-//HOMEPAGE
+//HOME
 app.get("/urls", (req, res) => {
-  const { username } = req.cookies;
+  const { username, user_id } = req.cookies;
+  console.log(req.cookies);
+
   const templateVars = {
     username,
     urls: urlDatabase,
+    users,
+    user: users[user_id],
   };
 
   res.render("urls_index", templateVars);
@@ -40,18 +62,48 @@ app.get("/urls", (req, res) => {
 
 //CREATE NEW URL PAGE
 app.get("/urls/new", (req, res) => {
-  const { username } = req.cookies;
+  const { username, users } = req.cookies;
+
   const templateVars = {
     username,
+    users,
   };
   res.render("urls_new", templateVars);
 });
 
-//EDIT PAGE
+//REGISTER
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
+
+//CREATE USER
+app.post("/register/", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(401).send("Invalid password and email");
+
+  const id = generateRandomString(6);
+
+  const newUser = { [id]: { user_id: id, email, password } };
+
+  Object.assign(users, newUser);
+
+  res.cookie("user_id", id);
+
+  res.redirect("/urls");
+});
+
+// EDIT PAGE
 app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
-  const { username } = req.cookies;
-  const templateVars = { id, longURL: urlDatabase[id], username };
+  const { username, users } = req.cookies;
+
+  if (!urlDatabase[id]) {
+    return res.status(404).send("ID CANT BE FOUND");
+  }
+
+  const templateVars = { id, longURL: urlDatabase[id], username, users };
   res.render("urls_show", templateVars);
 });
 
@@ -78,7 +130,6 @@ app.post("/urls/", (req, res) => {
   const { longURL } = req.body;
   Object.assign(urlDatabase, { [id]: longURL });
 
-  // res.redirect(`urls/${id}`);
   res.redirect(`/urls/${id}`);
 });
 
@@ -109,25 +160,9 @@ app.post("/login", (req, res) => {
 //LOGOUT
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
